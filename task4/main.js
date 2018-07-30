e27fc048e1c279b13e6d0383fd8672657cc4a871
Ihor Andriiv <andriivihor@icloud.com>
2018-07-30 22:48:29 +0300

    edited task 4

--- task4/main.js
+++ task4/main.js
@@ -6,9 +6,8 @@ function getCurrentLocation() {
 function showPosition(position) {
     let latLon = position.coords.latitude + "," + position.coords.longitude;
     let imgUrl = "https://maps.googleapis.com/maps/api/staticmap?center="
-    +latLon+"&zoom=18&size=400x300&key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU";
+    +latLon+"&zoom=18&size=400x300&key=AIzaSyAOK1yMMCD-xdJL8BaVpgTO6TwMXNtKX3A";
     document.getElementById("mapholder").innerHTML = "<img src='"+imgUrl+"'>";
 }
-
 
 
